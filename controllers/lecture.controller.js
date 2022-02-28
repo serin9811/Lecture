bbf7dabc1273c1203addd9fdd6d227f8ef572e86
serinHeo <serin9811@gmail.com>
2022-02-28 20:43:20 +0900

    Fix: fix name of functions

--- controllers/lecture.controller.js
+++ controllers/lecture.controller.js
@@ -1,5 +1,17 @@
 const Lecture = require("../models/lecture.model.js");
 
+exports.selectList = async (req, res) => {
+  try {
+    const lectureList = await Lecture.selectList(req);
+    if (!lectureList.length) {
+      res.status(404).json({ message: "No lecture" });
+    }
+    res.json(lectureList);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 exports.selectOne = async (req, res) => {
   if (!req.param) {
     res.status(400).send({
@@ -9,44 +21,12 @@ exports.selectOne = async (req, res) => {
   }
 
   try {
-    // Select lecture in database
-    const selectResult = await Lecture.selectIdx(req.params.lectureIdx);
+    const selectResult = await Lecture.selectOne(req.params.lectureIdx);
 
     if (!selectResult.length) {
       res.status(404).json({ message: "No such lecture" });
     }
     res.json(selectResult[0]);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-exports.update = (req, res) => {
-  let lectureIdx = req.params.lectureIdx;
-  if (!req.body || !req.params) {
-    res.status(400).send({
-      message: "Parameter or Contents cannot be empty",
-    });
-    return;
-  }
-
-  Lecture.update(lectureIdx, req.body, (err) => {
-    if (err) res.status(500).send({ message: err.message });
-    else
-      return res.send({
-        resultCode: 200,
-        resultMessage: req.params.lectureIdx + " lecture updated successfully",
-      });
-  });
-};
-
-exports.selectList = async (req, res) => {
-  try {
-    const lectureList = await Lecture.selectList(req);
-    if (!lectureList.length) {
-      res.status(404).json({ message: "No lecture" });
-    }
-    res.json(lectureList[0]);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -81,5 +61,51 @@ exports.create = async (req, res) => {
     res.json(createResult[0]);
   } catch (err) {
     res.status(500).json({ message: err });
+  }
+};
+
+exports.update = (req, res) => {
+  let lectureIdx = req.params.lectureIdx;
+  if (!req.body || !req.params) {
+    res.status(400).send({
+      message: "Parameter or Contents cannot be empty",
+    });
+    return;
+  }
+
+  Lecture.update(lectureIdx, req.body, (err) => {
+    if (err) res.status(500).send({ message: err.message });
+    else
+      return res.send({
+        resultCode: 200,
+        resultMessage: req.params.lectureIdx + " lecture updated successfully",
+      });
+  });
+};
+
+exports.patch = async (req, res) => {
+  /*
+  Binding doesn't work! 
+  const lecture = new Lecture({
+    lectureIdx: req.params.lectureIdx,
+    isShowYN: req.body.isShowYN,
+  });
+  */
+  try {
+    /*
+    console.log("lectureIdx : " + req.params.lectureIdx);
+    console.log("isShowYN : " + req.body.isShowYN);
+    */
+    console.log("isShowYN: " + req.body.isShowYN);
+    console.log("lectureIdx: " + req.params.lectureIdx);
+
+    const openedLecture = await Lecture.open(
+      req.body.isShowYN,
+      req.params.lectureIdx
+    );
+
+    console.log("openedLecture: " + openedLecture);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- models/lecture.model.js
+++ models/lecture.model.js
@@ -1,16 +1,30 @@
 const { pQuery } = require("../utils");
-
+/*
 const Lecture = function (lecture) {
-  this.categoryIdx = lecture.categoryIdx;
-  this.teacherIdx = lecture.teacherIdx;
-  this.lectureTitle = lecture.lectureTitle;
-  this.lectureDesc = lecture.lectureDesc;
-  this.lecturePrice = lecture.lecturePrice;
-  this.isShowYN = lecture.isShowYN;
+  this.categoryIdx = lecture.categoryIdx; // 강의 카테고리 인덱스 번호
+  this.lectureTitle = lecture.lectureTitle; // 강의명
+  this.teacherIdx = lecture.teacherIdx; // 강사 인덱스 번호
+  this.lecturePrice = lecture.lecturePrice; // 수강료
+  this.lectureDesc = lecture.lectureDesc; // 강의 소개
+  this.isShowYN = lecture.isShowYN; // 강의 노출 여부(Y/N)
+  this.registDate = lecture.registDate; // 강의 생성일시
+  this.modifyDate = lecture.modifyDate; // 강의 수정일시
+};
+*/
+const Lecture = function (lecture) {}; // what is the difference?
+
+Lecture.selectList = async () => {
+  //검색어에 1. 강사명 2.강의이름 3.수강생ID
+  //검색조건에 1.강의카테고리 2.전체카테고리
+  //정렬: 최신순, 수강생 순
+  //let { category, teacher, title, student, order, page, pageSize } = req.query;
+  return await pQuery("SELECT * FROM tb_lecture");
 };
 
+Lecture.selectOne = async (lectureIdx) =>
+  pQuery("SELECT * FROM tb_lecture WHERE `lectureIdx` = ?", lectureIdx);
+
 Lecture.create = async (newLecture) => {
-  console.log("newLecture: " + newLecture);
   try {
     const insertResult = await pQuery(
       "INSERT INTO tb_lecture SET ?",
@@ -22,24 +36,6 @@ Lecture.create = async (newLecture) => {
   }
 };
 
-Lecture.selectIdx = async (lectureIdx) =>
-  pQuery("SELECT * FROM tb_lecture WHERE `lectureIdx` = ?", lectureIdx);
-
-Lecture.selectList = async (req) => {
-  //검색어에 1. 강사명 2.강의이름 3.수강생ID
-  //검색조건에 1.강의카테고리 2.전체카테고리
-  //정렬: 최신순, 수강생 순
-  let { category, teacher, title, student, order, page } = req.query;
-  if (category !== undefined) {
-    pQuery("SELECT * FROM ");
-  }
-  /*
-  pQuery(
-    "SELECT * FROM tb_lecture WHERE `isShowYN` = 'Y' ORDER BY `lectureIdx`"
-  );
-  */
-};
-
 Lecture.update = (lectureIdx, targetLecture) => {
   pQuery(
     "UPDATE tb_lecture SET `categoryIdx` = ?, `lectureTitle` = ?, `lectureDesc` = ?, `lecturePrice` = ?, `modifyDate` = NOW() WHERE `lectureIdx` = ?",
@@ -47,6 +43,14 @@ Lecture.update = (lectureIdx, targetLecture) => {
     targetLecture.lectureTitle,
     targetLecture.lectureDesc,
     targetLecture.lecturePrice,
+    lectureIdx
+  );
+};
+
+Lecture.updateShowYN = (isShowYN, lectureIdx) => {
+  pQuery(
+    "UPDATE tb_lecture SET `isShowYN` = ? WHERE `lectureIdx` = ?",
+    isShowYN,
     lectureIdx
   );
 };
