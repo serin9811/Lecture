955523565125d5f5bee752fb913a32af2a0708f8
serinHeo <serin9811@gmail.com>
2022-02-28 20:42:30 +0900

    Update: Add version in Endpoint

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("common"));
-app.use("/", router);
+app.use("/v1", router);
 
 app.use(
   "/api-docs",
@@ -19,5 +19,5 @@ app.use(
 );
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}/api`);
 });
